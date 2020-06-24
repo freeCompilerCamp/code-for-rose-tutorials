acd53d21651dcad741da3c43cd5cc1ddd70b41d3
Justin Gosselin <27461459+vec4@users.noreply.github.com>
2020-06-24 12:55:32 -0400

    Add rose-intro source code
    This is the code contained within the tutorial "Rose - An Introduction".

---
+++ rose-intro/sampleInput.cxx
@@ -0,0 +1,17 @@
+// Example input file for ROSE tutorial
+#include <iostream>
+// Main function int main()
+
+typedef float Real;
+
+int main( )
+{
+  int x=0;
+  bool value = false ;
+
+  for (int i=0; i < 4; i++) {
+    int x;
+  }
+
+  return 0;
+}
