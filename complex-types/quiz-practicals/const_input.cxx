7deeab57df855c69ba03c49c86598ccb1f48dd43
Justin Gosselin <27461459+vec4@users.noreply.github.com>
2020-06-30 17:12:24 -0400

    Update const_input.cxx
    Make code actually able to compile :wink:

--- complex-types/quiz-practicals/const_input.cxx
+++ complex-types/quiz-practicals/const_input.cxx
@@ -1,8 +1,11 @@
+#include <cstdint>
+#include <cmath>
+
 const uint64_t foo = 632356;
 
 void bar() {
 	const float PI = 3.1415926;
-	x = atan2(3 * PI / 2);
+	float x = atan2(3 * PI, 2);
 }
 
 int main(int argc, char* argv[]) {
