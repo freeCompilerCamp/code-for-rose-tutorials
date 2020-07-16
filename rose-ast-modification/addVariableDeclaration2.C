4c9ed341afc8a2b0a51f3e05219a9b8c975ca414
Justin Gosselin <27461459+vec4@users.noreply.github.com>
2020-07-16 12:31:17 -0400

    Restructure directory and add new files for updated tutorial
    Restructing directory to be consistent with FreeCC.
    
    We make a slight modification to the variable declaration translator, which we place here.
    
    Also add the files for the new function calls section.

---
+++ rose-ast-modification/addVariableDeclaration2.C
@@ -0,0 +1,41 @@
+// SageBuilder contains all high level buildXXX() functions,
+// such as buildVariableDeclaration(), buildLabelStatement() etc.
+// SageInterface contains high level AST manipulation and utility functions,
+// e.g. appendStatement(), lookupFunctionSymbolInParentScopes() etc.
+#include "rose.h"
+using namespace SageBuilder;
+using namespace SageInterface;
+
+class SimpleInstrumentation:public SgSimpleProcessing
+{
+public:
+  void visit (SgNode * astNode);
+};
+
+void
+SimpleInstrumentation::visit (SgNode * astNode)
+{
+  SgBasicBlock *block = isSgBasicBlock (astNode);
+  if (block != NULL)
+    {
+      SgVariableDeclaration *variableDeclaration =
+                buildVariableDeclaration ("newVariable", buildIntType (), NULL, block);
+      prependStatement (variableDeclaration, block);
+    }
+}
+
+int
+main (int argc, char *argv[])
+{
+  // Initialize and check compatibility. See Rose::initialize
+  ROSE_INITIALIZE;
+
+  SgProject *project = frontend (argc, argv);
+  ROSE_ASSERT (project != NULL);
+
+  SimpleInstrumentation treeTraversal;
+  treeTraversal.traverseInputFiles (project, preorder);
+
+  AstTests::runAllTests (project);
+  return backend (project);
+}
---
+++ rose-ast-modification/inputbuildFunctionCalls.C
@@ -0,0 +1,7 @@
+void foo(int x);
+
+int main (void)
+{
+  int p_sum=0;
+  return p_sum;
+}
